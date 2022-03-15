8d4da0a47e7d44dcc220da4dbfc11014e199790a
DrDano <danielharned@gmail.com>
2022-03-15 14:40:20 -0700

    bundling code and replacing script tags in html with scripts referencing distribution file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {entry: './assets/js/script.js', 
 output: {
-    path: path.resolve(_dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js'
 },
 mode: 'development',
